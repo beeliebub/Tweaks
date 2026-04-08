5fe689a38550b01487ba0feea6df16f6cdc9eac2
beeliebub <tulpa.camellia@gmail.com>
2026-04-08 14:52:08 -0500

    Portal Event Fix

--- src/main/java/me/beeliebub/tweaks/EndPortalListener.java
+++ src/main/java/me/beeliebub/tweaks/EndPortalListener.java
@@ -2,6 +2,7 @@ package me.beeliebub.tweaks;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
 import java.util.List;
@@ -15,7 +16,7 @@ public class EndPortalListener implements Listener {
     }
     
     @EventHandler
-    public void onEnterPortal(PlayerTeleportEvent event) {
+    public void onEnterPortal(PlayerPortalEvent event) {
         if (event.getCause() == PlayerTeleportEvent.TeleportCause.END_PORTAL) {
             List<String> disabledWorlds = plugin.getConfig().getStringList("disabled-end-portal-worlds");
             if (disabledWorlds.contains(event.getPlayer().getWorld().getName())) {
